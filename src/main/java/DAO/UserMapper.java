8511127242418874bdc10f81eb88b0a8ceafd45a
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-05-23 21:59:06 +0400

    --one by one query

--- src/main/java/DAO/UserMapper.java
+++ src/main/java/DAO/UserMapper.java
@@ -2,12 +2,12 @@ package DAO;
 
 import Models.Gender;
 import Models.User;
+import lombok.extern.log4j.Log4j2;
 import org.apache.ibatis.annotations.*;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
-
 
 public interface UserMapper{
     String getAll = "SELECT * FROM \"user\"";
@@ -16,9 +16,11 @@ public interface UserMapper{
     String insert = "INSERT INTO \"user\" (USERNAME, PASSWORD, GENDER, IMAGEURL ) VALUES (#{username}, #{password}, #{gender}, #{imageURL})";
     String update = "UPDATE \"user\" SET USERNAME = #{username}, PASSWORD = #{password}, GENDER = #{gender}, IMAGEURL = #{imageURL}, LASTLOGIN=#{lastLogin,typeHandler=org.apache.ibatis.type.LocalDateTimeTypeHandler} WHERE ID = #{userID}";
     String getBY = "SELECT * FROM \"user\" WHERE USERNAME=#{username}";
-    String getUserOneByOne = "SELECT id, username, gender,imageURL, password FROM \"user\" u\n" +
+  /*  String getUserOneByOne = "SELECT id, username, gender,imageURL, password FROM \"user\" u\n" +
             "LEFT OUTER JOIN Liked l ON l.toUser != u.id\n" +
             "WHERE #{userID}=l.\"user\" AND u.id !=#{userID} limit 1";
+ */
+    String getUserOneByOne = "SELECT ID,USERNAME,PASSWORD,GENDER,LASTLOGIN,IMAGEURL FROM \"user\" u WHERE u.ID NOT IN( select l.touser from liked l where l.\"user\" = #{userID}) and u.id != #{userID } limit 1";
     @Select(getAll)
     @Results(value = {
             @Result(property = "userID",column = "id"),
