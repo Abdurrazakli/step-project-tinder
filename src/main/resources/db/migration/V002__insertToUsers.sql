75c6b7ca43096b50cf94c736d16fab6384b5ed60
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-05-23 13:08:48 +0400

    --database user creation

---
+++ src/main/resources/db/migration/V002__insertToUsers.sql
@@ -0,0 +1,4 @@
+INSERT INTO public."user" (username, password, gender, imageurl, lastlogin) VALUES ('nata', 'test', 'F', 'qlamurniyFoto', null);
+INSERT INTO public."user" (username, password, gender, imageurl, lastlogin) VALUES ('efqan', 'test', 'M', 'hfjhdfjhfjd', null);
+INSERT INTO public."user" (username, password, gender, imageurl, lastlogin) VALUES ('ayka', 'test', 'F', 'yaxciQizinFotosuOlmur', null);
+INSERT INTO public."user" (username, password, gender, imageurl, lastlogin) VALUES ('adil', 'test', 'M', 'dsdsd', null);
